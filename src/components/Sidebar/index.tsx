55019c84cc600da1f6871bc224a7e853c4a51cd8
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-06-02 18:17:21 +0300

    .

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -97,9 +97,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
         <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6">
           {/* <!-- Menu Group --> */}
           <div>
-            <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
+            {/* <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
               MENU
-            </h3>
+            </h3> */}
             <ul className="mb-6 flex flex-col gap-1.5">
               {/* <!-- Menu Item Dashboard --> */}
               <SidebarLinkGroup
