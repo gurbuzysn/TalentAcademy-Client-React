6e642703fd443a55f68ab45f37c02e0cf1b5b63e
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-06-03 14:36:23 +0300

    Student Listesi Düzenlendi

--- src/pages/Student/StudentList.tsx
+++ src/pages/Student/StudentList.tsx
@@ -1,119 +1,235 @@
-import { Product } from '../../types/product';
-import ProductOne from '../../images/product/product-01.png';
-import ProductTwo from '../../images/product/product-02.png';
-import ProductThree from '../../images/product/product-03.png';
-import ProductFour from '../../images/product/product-04.png';
-
 import DefaultLayout from '../../layout/DefaultLayout';
+import { useEffect, useState } from 'react';
 
+function StudentList() {
 
+const [students, setStudents] = useState([])
 
-function StudentList(){
+  useEffect(() => {
+    fetch('https://localhost:7043/api/Students')
+      .then((res) => res.json())
+      .then((data) => {
+        setStudents(data);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
+ 
 
+  return (
+    <DefaultLayout>
+      <>
 
-    const productData: Product[] = [
-      {
-        image: ProductOne,
-        name: 'Apple Watch Series 7',
-        category: 'Electronics',
-        price: 296,
-        sold: 22,
-        profit: 45,
-      },
-      {
-        image: ProductTwo,
-        name: 'Macbook Pro M1',
-        category: 'Electronics',
-        price: 546,
-        sold: 12,
-        profit: 125,
-      },
-      {
-        image: ProductThree,
-        name: 'Dell Inspiron 15',
-        category: 'Electronics',
-        price: 443,
-        sold: 64,
-        profit: 247,
-      },
-      {
-        image: ProductFour,
-        name: 'HP Probook 450',
-        category: 'Electronics',
-        price: 499,
-        sold: 72,
-        profit: 103,
-      },
-    ];
+      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="py-6 px-4 md:px-6 xl:px-7.5">
+           <h4 className="text-xl font-semibold text-black dark:text-white">
+             Öğrenci Listesi
+           </h4>
+         </div>
 
+        <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
 
-
-
-
-    return(
-        <DefaultLayout>
-        <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div className="py-6 px-4 md:px-6 xl:px-7.5">
-        <h4 className="text-xl font-semibold text-black dark:text-white">
-          Top Products
-        </h4>
-      </div>
-
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-3 flex items-center">
-          <p className="font-medium">Product Name</p>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium">Category</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Price</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Sold</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Profit</p>
-        </div>
-      </div>
-
-      {productData.map((product, key) => (
-        <div
-          className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
-          key={key}
-        >
-          <div className="col-span-3 flex items-center">
-            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-              <div className="h-12.5 w-15 rounded-md">
-                <img src={product.image} alt="Product" />
-              </div>
-              <p className="text-sm text-black dark:text-white">
-                {product.name}
-              </p>
-            </div>
+           <div className="col-span-2 flex items-center">
+            <p className="font-medium">Profil Fotoğrafı</p>
           </div>
+
+          <div className="col-span-1 hidden items-center sm:flex">
+             <p className="font-medium">İsim</p>
+          </div>
+          
           <div className="col-span-2 hidden items-center sm:flex">
-            <p className="text-sm text-black dark:text-white">
-              {product.category}
-            </p>
+             <p className="font-medium">Last Name</p>
           </div>
+
           <div className="col-span-1 flex items-center">
-            <p className="text-sm text-black dark:text-white">
-              ${product.price}
-            </p>
+            <p className="font-medium">Gender</p>
           </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-black dark:text-white">{product.sold}</p>
+          
+         <div className="col-span-1 flex items-center">
+            <p className="font-medium">Date of Birth</p>
+           </div>
+
+           <div className="col-span-1 flex items-center">
+             <p className="font-medium">Id</p>
           </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-meta-3">${product.profit}</p>
+
+         </div>
+
+        {students.map((student, key) => ( 
+           <div
+             className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
+             key={key}
+           >
+
+
+
+             <div className="col-span-2 flex items-center">
+               <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+                 <div className="h-15 w-15">
+                   <img className='rounded-full' src={student.imageUri ? student.imageUri : "https://localhost:7043/images/blank-profile-photo.jpg"} alt="Product" />
+                 </div>
+              </div>
+            </div>
+
+
+            <div className="col-span-1 hidden items-center sm:flex">
+               <p className="text-sm text-black dark:text-white">
+                 {student.firstName}
+               </p>
+             </div>
+
+
+
+             <div className="col-span-2 hidden items-center sm:flex">
+               <p className="text-sm text-black dark:text-white">
+                 {student.lastName}
+               </p>
+             </div>
+
+
+
+             <div className="col-span-1 flex items-center">
+               <p className="text-sm text-black dark:text-white">
+                 {student.gender === 1 ? 'Kadın' : 'Erkek'}
+               </p>
+             </div>
+
+
+
+             <div className="col-span-1 flex items-center">
+               <p className="text-sm text-black dark:text-white">
+                 {new Date(student.dateOfBirth).toLocaleDateString()}
+               </p>
+           </div>
+           <div className="col-span-1 flex items-center">
+               <p className="text-sm text-meta-3">${student.id}</p>
+             </div>
           </div>
-        </div>
-      ))}
-    </div>
-        </DefaultLayout>
-    )
+         ))}
+       </div>
+
+
+      </>
+    </DefaultLayout>
+  );
 }
 
-export default StudentList
+export default StudentList;
+
+
+
+
+
+
+// import { Product } from '../../types/product';
+// import ProductOne from '../../images/product/product-01.png';
+// import ProductTwo from '../../images/product/product-02.png';
+// import ProductThree from '../../images/product/product-03.png';
+// import ProductFour from '../../images/product/product-04.png';
+
+
+ // const productData: Product[] = [
+  //   {
+  //     image: ProductOne,
+  //     name: 'Apple Watch Series 7',
+  //     category: 'Electronics',
+  //     price: 296,
+  //     sold: 22,
+  //     profit: 45,
+  //   },
+  //   {
+  //     image: ProductTwo,
+  //     name: 'Macbook Pro M1',
+  //     category: 'Electronics',
+  //     price: 546,
+  //     sold: 12,
+  //     profit: 125,
+  //   },
+  //   {
+  //     image: ProductThree,
+  //     name: 'Dell Inspiron 15',
+  //     category: 'Electronics',
+  //     price: 443,
+  //     sold: 64,
+  //     profit: 247,
+  //   },
+  //   {
+  //     image: ProductFour,
+  //     name: 'HP Probook 450',
+  //     category: 'Electronics',
+  //     price: 499,
+  //     sold: 72,
+  //     profit: 103,
+  //   },
+  // ];
+
+
+
+// <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+//         <div className="py-6 px-4 md:px-6 xl:px-7.5">
+//           <h4 className="text-xl font-semibold text-black dark:text-white">
+//             Top Products
+//           </h4>
+//         </div>
+
+//         <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
+
+//           <div className="col-span-3 flex items-center">
+//             <p className="font-medium">Product Name</p>
+//           </div>
+          
+//           <div className="col-span-2 hidden items-center sm:flex">
+//             <p className="font-medium">Category</p>
+//           </div>
+
+//           <div className="col-span-1 flex items-center">
+//             <p className="font-medium">Price</p>
+//           </div>
+          
+//           <div className="col-span-1 flex items-center">
+//             <p className="font-medium">Sold</p>
+//           </div>
+
+//           <div className="col-span-1 flex items-center">
+//             <p className="font-medium">Profit</p>
+//           </div>
+
+//         </div>
+
+//         {productData.map((product, key) => (
+//           <div
+//             className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
+//             key={key}
+//           >
+//             <div className="col-span-3 flex items-center">
+//               <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+//                 <div className="h-12.5 w-15 rounded-md">
+//                   <img src={product.image} alt="Product" />
+//                 </div>
+//                 <p className="text-sm text-black dark:text-white">
+//                   {product.name}
+//                 </p>
+//               </div>
+//             </div>
+//             <div className="col-span-2 hidden items-center sm:flex">
+//               <p className="text-sm text-black dark:text-white">
+//                 {product.category}
+//               </p>
+//             </div>
+//             <div className="col-span-1 flex items-center">
+//               <p className="text-sm text-black dark:text-white">
+//                 ${product.price}
+//               </p>
+//             </div>
+//             <div className="col-span-1 flex items-center">
+//               <p className="text-sm text-black dark:text-white">
+//                 {product.sold}
+//               </p>
+//             </div>
+//             <div className="col-span-1 flex items-center">
+//               <p className="text-sm text-meta-3">${product.profit}</p>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
